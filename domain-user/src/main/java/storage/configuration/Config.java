6edf962dfe4b099e60e0c71c04eadde4095e486e
kyj0101 <fioonmp1001@gmail.com>
2024-06-02 21:10:05 +0900

    add module

---
+++ domain-user/src/main/java/storage/configuration/Config.java
@@ -0,0 +1,7 @@
+package storage.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+}
---
+++ domain-user/src/main/java/storage/persistence/entity/User.java
@@ -0,0 +1,4 @@
+package storage.persistence.entity;
+
+public class User {
+}
