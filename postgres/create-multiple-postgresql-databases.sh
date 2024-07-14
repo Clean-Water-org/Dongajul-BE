2cae09a1b7644baf8348266bf1e0383bd9890baf
MinseoRhie <wiwid@irkudos.co.kr>
2024-07-14 21:19:20 +0900

    Postgresql 설정 오류 수정 및 연동 오류 수정

--- postgres/create-multiple-postgresql-databases.sh
+++ postgres/create-multiple-postgresql-databases.sh
@@ -8,9 +8,11 @@ function create_user_and_database() {
 	local ownerName=$(echo $1 | tr ',' ' ' | awk  '{print $2}')
 	local ownerPassword=$(echo $1 | tr ',' ' ' | awk  '{print $3}')
 	echo "  Creating user and database '$database'"
+	echo "  -e user=$ownerName"
+	echo "  -e password=$ownerPassword"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 	    CREATE USER $ownerName;
-	    CREATE DATABASE $database;
+	    CREATE DATABASE $database WITH OWNER $ownerName;
 	    ALTER USER $ownerName WITH PASSWORD '$ownerPassword';
 	    GRANT ALL PRIVILEGES ON DATABASE $database TO $ownerName;
 EOSQL
