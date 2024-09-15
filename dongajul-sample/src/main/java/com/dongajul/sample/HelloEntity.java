263451999d376de7057c5980926851e3d631ba7f
kyj0101 <fioonmp1001@gmail.com>
2024-09-15 13:24:33 +0900

    discovery 설정 저장

--- dongajul-sample/src/main/java/com/dongajul/sample/HelloEntity.java
+++ dongajul-sample/src/main/java/com/dongajul/sample/HelloEntity.java
@@ -1,25 +1,21 @@
 package com.dongajul.sample;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.*;
 
-@Getter
-@Setter
-@ToString(callSuper = true)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Entity
-@Table
+//@Getter
+//@Setter
+//@ToString(callSuper = true)
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@AllArgsConstructor
+//@Entity
+//@Table
 public class HelloEntity {
-
-    @Id
-    @GeneratedValue
-    private Long id;
-
-    private String name;
-    private int age;
-    private String message;
+//
+//    @Id
+//    @GeneratedValue
+//    private Long id;
+//
+//    private String name;
+//    private int age;
+//    private String message;
 }
--- dongajul-sample/src/main/java/com/dongajul/sample/SampleApplication.java
+++ dongajul-sample/src/main/java/com/dongajul/sample/SampleApplication.java
@@ -2,8 +2,10 @@ package com.dongajul.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(SampleApplication.class, args);
